e7e6017df803d47a1b7f1adfcba3386f908079aa
fatihabr <fatiha@gmail.com>
2023-03-02 14:47:49 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,30 @@
 #include "holberton.h"
 /**
- * leet - encodes a string
- * @str: the string to encode
+ * cap_string - capitalizes all words of a string
+ * @str: the string to change the first letter of a word in uppercase
  *
- * Return: the encode string
+ * Return: capitalizes letters
  */
-char *leet(char *str)
+char *cap_string(char *str)
 {
-	int i, j;
-	char c[] = "aAeEoOtTlL";
-	char n[] = "4433007711";
+	int i;
+	int j;
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
 	i = 0;
+
 	while (str[i] != '\0')
 	{
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
 		j = 0;
 		while (c[j] != '\0')
 		{
-			if (str[i] == c[j])
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
 			{
-				str[i] = n[j];
+				str[i + 1] = str[i + 1] - 32;
 			}
 			j++;
 		}
