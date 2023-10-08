ca2017bf7e203073c9e10f60d405ac6da532f17a
fatiha bourjdal <fatihabourjdal7@gmail.com>
2023-10-08 16:02:53 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,15 +5,13 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
  * Return: Nothing.
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
-	unsigned int i, max = new_size;
-	char *oldp = ptr;
+	unsigned int i;
 	
 	if (ptr == NULL)
 	{
@@ -21,27 +19,29 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (p);
 
 	}
-	else if (new_size == 0)
-	{
+	else if (new_size == old_size)
+	
+		return (ptr);
+	
+	else if (new_size == 0 && ptr != NULL)
+	{ 
 		free(ptr);
 		return (NULL);
 	}
-	else if (new_size == old_size)
-	{ 
-		return (ptr);
-	}
-	if (p == NULL)
+	else
 	{
-		return (NULL);
-	}
-	if (new_size > old_size)
+		p = malloc(new_size);
+
+	if (p != NULL)
 	    {
-		    max = old_size;
-	    }
-        for (i = 0; i < max; i++)
-	{
-		p[i] = oldp[i];
+	    
+           for (i = 0; i < min(old_size, new_size); i++)
+
+	         *((char *)p + i) = *((char *) ptr + i);
 		free(ptr);
 		return (p);
-	}
+	    }
+	else
+		return (NULL);
+
 }
