bb0d644a760d816b1532db60fc563484198f0ba5
fatiha bourjdal <fatihabourjdal7@gmail.com>
2023-10-08 05:56:38 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * malloc_checked -main Entry
+ * @b: input
+ * Return: 0
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+
+}
